b633c9697c904f4e5444f78e79a103301238eaa3
mminhaaj <minhaaj768@gmail.com>
2016-01-19 10:58:46 +0530

    update publish

--- server/pubslish.js
+++ server/pubslish.js
@@ -5,3 +5,15 @@ Meteor.publish('advancelevelsubjects', function() {
 Meteor.publish('candidates', function() {
 	return Candidates.find({});
 });
+
+Meteor.publish('divisions', function() {
+	return Divisions.find({});
+});
+
+Meteor.publish('gramasevavasam', function() {
+	return Gramasevavasam.find({});
+});
+
+Meteor.publish('poolingbooth', function() {
+	return Poolingbooths.find({});
+});
